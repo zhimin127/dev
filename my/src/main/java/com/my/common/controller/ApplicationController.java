f81a12d67fab3a9bc793cb837746c18698736019
zhimin <zhimin@zhimin-pc>
2015-03-09 14:27:11 +0800

    spring mvc 与淘宝Kissy editor 图片上传

---
+++ my/src/main/java/com/my/common/controller/ApplicationController.java
@@ -0,0 +1,14 @@
+package com.my.common.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class ApplicationController {
+
+	@RequestMapping("error.html")
+	public ModelAndView error(){
+		return new ModelAndView("error");
+	}
+}
