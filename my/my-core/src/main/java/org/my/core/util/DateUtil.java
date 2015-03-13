784e21ac8376635f25788433e2a4e9c79e69be6c
zhimin <zhimin127@sina.com>
2015-03-13 18:42:29 +0800

    util

---
+++ my/my-core/src/main/java/org/my/core/util/DateUtil.java
@@ -0,0 +1,18 @@
+package org.my.core.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+	public static final String SHORT_FORMAT = "yyyy-MM-dd";
+	public static final String LONG_FORMAT = "yyyy-MM-dd hh:mm:ss";
+	
+	public static String getShortDate(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat(SHORT_FORMAT);
+		if (null == date || "".equals(date)) {
+			date = new Date();
+		}
+		return sdf.format(date);
+	}
+}
---
+++ my/my-core/src/main/java/org/my/core/util/GsonUtil.java
@@ -0,0 +1,37 @@
+package org.my.core.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * 
+ * @author zhimin
+ *@version
+ */
+public class GsonUtil {
+
+	/**
+	 * user gson format object to json
+	 * 
+	 * @param object
+	 * @return json
+	 */
+	public static String toJson(Object object) {
+		Gson gson = new Gson();
+		return gson.toJson(object);
+	}
+
+	/**
+	 * user gson format object to json and Without Expose
+	 * 
+	 * @param object
+	 * @return json string
+	 */
+	public static String toJsonWithoutExpose(Object object) {
+		Gson gson = new Gson();
+		GsonBuilder gb = new GsonBuilder();
+		gb.excludeFieldsWithoutExposeAnnotation();
+		gson = gb.create();
+		return gson.toJson(object);
+	}
+}
